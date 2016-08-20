f850198f442d40e0e75f5686f276b6b8deac6cc2
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2016-08-20 11:07:35 -0300

    Add /videos route

--- server/routes/loadRoutes.js
+++ server/routes/loadRoutes.js
@@ -1,12 +1,14 @@
 var express = require('express');
 
 var SpeechRoute = require(__base + 'routes/speechRoute');
+var VideosRoute = require(__base + 'routes/videosRoute');
 
 var LoadRoutes = function(){
 
 	router = express.Router();
 
 	new SpeechRoute(router);
+	new VideosRoute(router);
 	return router;
 }
 
