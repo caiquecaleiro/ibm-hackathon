63abd7b47a195c1b4a6ae74a7b4a57bbe323e900
Caique Caleiro <caiquecaleiro@gmail.com>
2016-08-20 13:47:44 -0300

    Add new video id

--- public/src/app/converter/converter.controller.js
+++ public/src/app/converter/converter.controller.js
@@ -15,6 +15,8 @@
     vm.videos = [];
     vm.keywords = [];
 
+    createVideos();
+
     function submitAction(url, keywords) {
       keywords = keywords.replace(/\s*,\s*/g, ',');
       var json = {
@@ -25,12 +27,19 @@
     }
 
     function createKeywords(keywords) {
-      converterService.getKeywords(keywords)
+      converterService.getKeywords(keywords, vm.videos)
         .then(function(data) {
           vm.keywords = data;
         });
       // var vid = document.getElementById("myVideo");
       // vid.currentTime = 5;
+    }
+
+    function createVideos() {
+      converterService.getVideos()
+        .then(function(data) {
+          vm.videos = data;
+        });
     }
   }
 })();
--- public/src/app/converter/converter.service.js
+++ public/src/app/converter/converter.service.js
@@ -9,46 +9,68 @@
 
   function converterService($http, $q) {
     var service = {
-      getKeywords: getKeywords
+      getKeywords: getKeywords,
+      getVideos: getVideos
     };
     return service;
 
-    function getKeywords(keywords) {
+    function getKeywords(keywords, videos) {
       var deferred = $q.defer();
-      $http.post('http://10.1.37.151:8080/api/speech/' + 'Aula1', keywords)
-        .success(function(data) {
-          var keywords = [];
-          for (var object in data) {
-            data[object].forEach(function(word) {
-              var keyword = new Keyword(
-                word.normalized_text,
-                word.start_time
-              );
-              keywords.push(keyword);
-            });
-          };
-          deferred.resolve(keywords);
+      videos.forEach(function(object) {
+        $http.post('http://localhost:8080/api/speech/' + object.id, keywords)
+          .success(function(data) {
+            var keywords = [];
+            for (var object in data) {
+              data[object].forEach(function(word) {
+                var keyword = new Keyword(
+
+                  word.normalized_text,
+                  word.start_time
+                );
+                keywords.push(keyword);
+              });
+            }
+            deferred.resolve(keywords);
+          })
+          .catch(function(error) {
+            deferred.reject(error);
+          });
+      });
+      return deferred.promise;
+    }
+
+    function getVideos() {
+      var deferred = $q.defer();
+      var videos = [];
+      $http.get('http://localhost:8080/api/videos')
+        .then(function(response) {
+          response.data.forEach(function(object) {
+            var video = new Video(
+              object.id,
+              object.path,
+              object.id
+            );
+            videos.push(video);
+          });
+          deferred.resolve(videos);
         })
         .catch(function(error) {
-          console.log(error.data.message);
-          return deferred.reject(error);
+          deferred.reject(error);
         });
-        return deferred.promise;
+      return deferred.promise;
     }
 
     /**
      * A Video object.
+     * @param {String} id - The video id.
      * @param {String} url - The video URL.
-     * @param {String} title - The video title.
-     * @param {String} image - The image link.
      * @param {String} time - The video total time.
      * @constructor
      */
-    function Video(url, title, image, time) {
+    function Video(id, url, title) {
+      this.id = id;
       this.url = url;
       this.title = title;
-      this.image = image;
-      this.time = time;
     }
 
     /**
