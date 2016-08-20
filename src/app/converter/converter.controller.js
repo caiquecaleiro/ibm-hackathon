7279fa2a3caa052403d9acbbee424e6422ac6ebb
Caique Caleiro <caiquecaleiro@gmail.com>
2016-08-20 11:16:49 -0300

    Add getKeywords to create the keyword object

--- src/app/converter/converter.controller.js
+++ src/app/converter/converter.controller.js
@@ -13,49 +13,24 @@
     vm.url = '';
     vm.keyword = '';
     vm.videos = [];
+    vm.keywords = [];
 
     function submitAction(url, keywords) {
-      console.log(keywords.split(','));
-      createVideos();
+      keywords = keywords.replace(/\s*,\s*/g, ',');
+      var json = {
+        'keywords': keywords.split(',')
+      };
+      json = JSON.stringify(json);
+      createKeywords(json);
     }
 
-    function createVideos(data) {
-      //Test
-      var video = new Video('https://www.youtube.com/',
-        'test1',
-        'http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg',
-        '3:00');
-      vm.videos.push(video);
-      //
-      // converterService.getData()
-      //   .then(function(data) {
-      //     console.log(data);
-      //   });
-      // vm.videos = null;
-      // data.foreach(function(object) {
-      //   var video = new Video(
-      //     data.url,
-      //     data.title,
-      //     data.image,
-      //     data.time
-      //   );
-      //   vm.videos.push(video);
-      //});
-    }
-
-    /**
-     * A Video object.
-     * @param {String} url - The video URL.
-     * @param {String} title - The video title.
-     * @param {String} image - The image link.
-     * @param {String} time - The video total time.
-     * @constructor
-    */
-    function Video(url, title, image, time) {
-      this.url = url;
-      this.title = title;
-      this.image = image;
-      this.time = time;
+    function createKeywords(keywords) {
+      converterService.getKeywords(keywords)
+        .then(function(data) {
+          vm.keywords = data;
+        });
+      // var vid = document.getElementById("myVideo");
+      // vid.currentTime = 5;
     }
   }
 })();
